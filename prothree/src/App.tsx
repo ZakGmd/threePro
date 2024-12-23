4d8e4313fecb07462131fcb571876c92d044851a
Ghoumidate zakaria <ghoumidatezakaria@gmail.com>
2024-12-23 22:49:45 +0100

    adjusting the 3d model and adding the lights to the scene and also some helpers to visualize the model , it looks so great now . i love it !

--- prothree/src/App.tsx
+++ prothree/src/App.tsx
@@ -1,14 +1,15 @@
 
 
 import './App.css'
+import Scene from './components/scene'
 
 function App() {
  
 
   return (
     <>
-     <div className='flex items-start gap-10 h-[100vh] w-full '>
-      <div className='h-full ml-[168px] pl-[32px] pr-[28px] border-r border-l border-gray-300 '>
+     <div className='flex items-start gap-[10] h-[100vh] w-full '>
+      <div className='h-full w-[700px] ml-[148px] pl-[32px] pr-[28px] border-r border-l border-gray-300 '>
         <div className='flex flex-col items-baseline pt-[160px] gap-[180px]'>
            <div className='flex flex-col items-baseline gap-2'>
             <div className='text-black/60 text-[14px] font-normal uppercase leading-normal font-space-mono '>Your comfort matters.</div>
@@ -16,26 +17,26 @@ function App() {
            </div>
            <div className='flex flex-col items-  gap-2 w-full'>
             <div className='text-black/60 text-[14px] font-normal uppercase leading-normal  font-space-mono'>Choose your Model</div>
-            <div className='flex flex-col items-start  gap-3 w-full'>
+            <div className='flex flex-col items-start  gap-3 w-full font-inter'>
               <div className='flex items-center gap-4 py-3 px-4 bg-[#F5F5F5] rounded-[6px] w-full  '>
                 <input type="radio" name="inpt" id="redio" />
                 <div className='flex flex-col items-baseline gap-2'>
-                  <div className='text-[16px] leading-5'>Monkey Sofa — Limited edition</div>
-                  <div>3.5m x 2.4m with custom colors</div>
+                  <div className='text-[16px] leading-5 font-normal tracking-[-0.16px]'>Monkey Sofa — Limited edition</div>
+                  <div className='text-[13px] font-normal leading-normal tracking-[-0.13px] text-black/70'>3.5m x 2.4m with custom colors</div>
                 </div>
               </div>
               <div className='flex items-center gap-4 py-3 px-4 bg-[#F5F5F5] rounded-[6px] w-full  '>
                 <input type="radio" name="inpt" id="redio" />
                 <div className='flex flex-col items-baseline gap-2'>
-                  <div className='text-[16px] leading-5'>Monkey Sofa — Limited edition</div>
-                  <div>3.5m x 2.4m with custom colors</div>
+                  <div className='text-[16px] leading-5 font-normal tracking-[-0.16px]'>Monkey Sofa — Limited edition</div>
+                  <div className='text-[13px] font-normal leading-normal tracking-[-0.13px] text-black/70'>3.5m x 2.4m with custom colors</div>
                 </div>
               </div>
               <div className='flex items-center gap-4 py-3 px-4 bg-[#F5F5F5] rounded-[6px] w-full  '>
                 <input type="radio" name="inpt" id="redio" />
                 <div className='flex flex-col items-baseline gap-2'>
-                  <div className='text-[16px] leading-5'>Monkey Sofa — Limited edition</div>
-                  <div>3.5m x 2.4m with custom colors</div>
+                  <div className='text-[16px] leading-5 font-normal tracking-[-0.16px]'>Monkey Sofa — Limited edition</div>
+                  <div className='text-[13px] font-normal leading-normal tracking-[-0.13px] text-black/70'>3.5m x 2.4m with custom colors</div>
                 </div>
               </div>
             </div>
@@ -48,9 +49,10 @@ function App() {
 
            </div>
         </div>
-
       </div>
-      <div>canvas</div>
+      <div className='w-full h-full'>
+        <Scene />
+      </div>
 
      </div>
      
