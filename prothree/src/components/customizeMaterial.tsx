40ea446a440b176b273b58e4fa4d8cd184853769
Ghoumidate zakaria <ghoumidatezakaria@gmail.com>
2024-12-26 02:28:42 +0100

    adding the ui for selecting the color of the seat , it looks great !

--- prothree/src/components/customizeMaterial.tsx
+++ prothree/src/components/customizeMaterial.tsx
@@ -24,9 +24,29 @@ import { CustomizeViewProps } from "../types/types";
               {[{name:'Wood' , color: ' #A1662F'}, {name:'Marble', color: ''},  {name:'Gold', color: ''}].map((frame) => (
                 <div
                   key={frame.name}
-                  className={`px-2 py-1  ${frame.name === 'Gold' ? 'px-[11px] hover:bg-[#d4af376e] ' : frame.name === 'Marble' ? 'hover:bg-[#e1e4e2]': 'hover:bg-[#a1662f83]'} hover:shadow-md font-normal rounded-full border cursor-pointer border-black/20 hover:border-black/40 duration-300 transition-all`}
+                  className={`px-2 py-1 text-[14px] font-inter  ${frame.name === 'Gold' ? 'px-[11px] hover:bg-[#d4af376e] ' : frame.name === 'Marble' ? 'hover:bg-[#e1e4e2]': 'hover:bg-[#a1662f5e]'} hover:shadow-md font-normal rounded-full border cursor-pointer border-black/20 hover:border-black/40 duration-300 transition-all`}
                
                 >{frame.name} </div> 
+              ))}
+            </div>
+          </div>
+          <div className='flex flex-col gap-4'>
+            <div className='text-black/60 text-[14px] font-normal uppercase leading-normal font-space-mono'>
+              Chose seat color 
+            </div>
+            <div className='flex items-center gap-3'>
+              {[{name:'Light Khaki' , color: ' #D6C9AF'}, {name:'Emerald Green', color: '#022c22'},  {name:'Dark Charcoal', color: '#0c0a09'}].map((frame) => (
+                <div
+                  key={frame.name}
+                  className={`flex items-center font-inter  gap-1 px-2 py-1  ${frame.name === 'Emerald Green' ? ' hover:bg-[#022c2238] ' : frame.name === 'Dark Charcoal' ? 'hover:bg-[#0c0a0938]': 'hover:bg-[#d6c9af67]'} hover:shadow-md font-normal rounded-full border cursor-pointer border-black/20 hover:border-black/40 duration-300 transition-all`}
+                  
+                >
+                 <div
+                  className='w-3 h-3   rounded-full'
+                  style={{ backgroundColor: frame.color }}
+                  />
+                  <div className="text-[14px]  leading-normal">{frame.name}</div>
+                </div> 
               ))}
             </div>
           </div>
