a68c3255cd1d698c5d579332d3edfc5e0c2c630a
Ghoumidate zakaria <ghoumidatezakaria@gmail.com>
2025-01-07 00:06:07 +0100

    adding react router to handle the navigation between our pages , and initialize the checkout component !

---
+++ prothree/src/components/checkOut.tsx
@@ -0,0 +1,10 @@
+import { CardItem } from '../types/types';
+
+
+interface CheckoutProps {
+  cartItems: CardItem[];
+}
+
+export default function CheckoutPage({ cartItems }: CheckoutProps){
+  
+};
--- prothree/src/components/landing.tsx
+++ prothree/src/components/landing.tsx
@@ -7,7 +7,7 @@ export default function Landing({onNext}:LandingProps) {
 
     return(
         <>
-          <div className="flex flex-col items-start ml-[120px] gap-10 ">
+          <div className="flex flex-col items-start ml-[120px]  gap-10 ">
             <div className="flex flex-col items-baseline gap-2">
                 <div className="text-black/60 text-[14px] font-normal uppercase leading-normal font-space-mono ">Design Your Heirloom</div>
                 <div className="text-[32px] tracking-[-0.32px] leading-normal font-inter ">Consider our sofas an heirloom piece <br /> that will last for generations.</div>
